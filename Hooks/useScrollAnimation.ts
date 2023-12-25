84685317e6d5be71e9d5c4a46279e2fd766096e2
murtazaisback <muhammadmurtazanasir90@gmail.com>
2023-12-25 17:41:06 +0300

    first comm

--- Hooks/useScrollAnimation.ts
+++ Hooks/useScrollAnimation.ts
@@ -8,23 +8,29 @@ interface ScrollAnimationHookResult {
 
 function useScrollAnimation(): ScrollAnimationHookResult {
   const [isVisible, setIsVisible] = useState<boolean>(false);
+  const [hasTriggered, setHasTriggered] = useState<boolean>(false);
   const ref = useRef<HTMLDivElement | null>(null);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (ref.current) {
+      if (!hasTriggered && ref.current) {
         const rect = ref.current.getBoundingClientRect();
         const windowHeight = window.innerHeight || document.documentElement.clientHeight;
-        
-        setIsVisible(rect.top <= windowHeight && rect.bottom >= 0);
+
+        // Trigger the animation if the element is in the viewport
+        if (rect.top <= windowHeight && rect.bottom >= 0) {
+          setIsVisible(true);
+          setHasTriggered(true); // Set the flag to true to avoid further triggers
+        }
       }
     };
 
     window.addEventListener('scroll', handleScroll);
+
     return () => {
       window.removeEventListener('scroll', handleScroll);
     };
-  }, []);
+  }, [hasTriggered]); // Add hasTriggered as a dependency to the useEffect hook
 
   return { ref, isVisible };
 }
