459f00f35b8047f6b0f863e49ed15d1ba20bc0de
murtazaisback <muhammadmurtazanasir90@gmail.com>
2024-01-03 16:54:25 +0300

    fi

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -44,26 +44,39 @@ export default function Home() {
           transition={{ duration: 1 }}
           className="header_section"
         >
-            <h1 >Event. Planning. <br /> Genius.</h1>
-            <div className="header_box">
-              <div className="header_info">
-                <p >Create an account for FREE </p>
-                <div>
-                  <Link href="/Sign_Up" className="btn" >Sign Up</Link>
-                </div>
-                <div className="header_text" >
-                  <p className="yellow_cr">We’re the All-in-One Platform you’ve been waiting for.</p>
-                  <p className="mb">Plus. No monthly or annual fees. </p>
-                </div>
+          <h1 >Event. Planning. <br /> Genius.</h1>
+          <div className="header_box">
+            <div className="header_info">
+              <p >Create an account for FREE </p>
+              <div>
+                <Link href="/Sign_Up" className="btn" >Sign Up</Link>
               </div>
+              <motion.div
+                style={{
+                  animation: 'scale-up-tl 1s forwards',
+                }}
+                className='Hero_imgs pc_none'
+              >
+                <div className="wapper_Hero_imgs">
+
+                  <Image src={Landingmain} alt='' className='Hero_img' />
+
+                  <Image src={Ellispse} alt="" className='back_hero_img' style={{ transform: `rotate(${rotation}deg)` }} />
+                </div>
+              </motion.div>
+
+              <div className="header_text" >
+                <p className="yellow_cr">We’re the All-in-One Platform you’ve been waiting for.</p>
+                <p className="mb">Plus. No monthly or annual fees. </p>
+              </div>
+            </div>
           </div>
         </motion.div>
-
         <motion.div
           style={{
             animation: 'scale-up-tl 1s forwards',
           }}
-          className='Hero_imgs'
+          className='Hero_imgs res_none'
         >
           <div className="wapper_Hero_imgs">
 
@@ -72,6 +85,7 @@ export default function Home() {
             <Image src={Ellispse} alt="" className='back_hero_img' style={{ transform: `rotate(${rotation}deg)` }} />
           </div>
         </motion.div>
+
 
       </section>
 
@@ -97,8 +111,8 @@ export default function Home() {
         <div className="show_section">
           <div className="mid_effect"></div>
           <div className="show_container">
-            
-                <SilderImages/>
+
+            <SilderImages />
 
 
 
@@ -108,11 +122,11 @@ export default function Home() {
               animate={{ opacity: inView2 ? 1 : 0, x: inView2 ? 0 : 100 }}  // Slide in when in view
               transition={{ duration: 0.5 }}
             >
-            <div className="show_info" >
-              <h2 className='res_none'>Create your events with ease.</h2>
-              <p>Need reserved seating for <br />your event? Not a <br />problem.</p>
-              <p>Use our built-in designer to <br /> customize your seating chart and  <br /> allow customers to select their<br />seats at checkout</p>
-            </div>
+              <div className="show_info" >
+                <h2 className='res_none'>Create your events with ease.</h2>
+                <p>Need reserved seating for <br />your event? Not a <br />problem.</p>
+                <p>Use our built-in designer to <br /> customize your seating chart and  <br /> allow customers to select their<br />seats at checkout</p>
+              </div>
             </motion.div>
           </div>
         </div>
@@ -126,57 +140,57 @@ export default function Home() {
           }}
         >
 
-        <div className="table_section">
-          <div className="table_container" >
-            <table>
-              <thead>
-                <tr>
-                  <th>Features</th>
-                  <th>Event Visionary</th>
-                  <th className="tc">Eventbrite</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>Create and List Events</td>
+          <div className="table_section">
+            <div className="table_container" >
+              <table>
+                <thead>
+                  <tr>
+                    <th>Features</th>
+                    <th>Event Visionary</th>
+                    <th className="tc">Eventbrite</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Create and List Events</td>
 
-                  <td className="tc"><FaCheck className="small_icon green" /></td>
-                  <td className="tc"><FaCheck className="small_icon green" /></td>
-                </tr>
-                <tr>
-                  <td>Sell Tickets</td>
-                  <td className="tc"><FaCheck className="small_icon green" /></td>
-                  <td className="tc"><FaCheck className="small_icon green" /></td>
-                </tr>
-                <tr>
-                  <td>Run SMS Campaigns</td>
-                  <td className="tc"><FaCheck className="small_icon green" /></td>
-                  <td className="tc"><FaXmark className="small_icon red" /></td>
-                </tr>
-                <tr>
-                  <td>Easily create custom dynamic seating charts</td>
-                  <td className="tc"><FaCheck className="small_icon green" /></td>
-                  <td className="tc"><FaXmark className="small_icon red" /></td>
-                </tr>
-                <tr>
-                  <td>Manage Orders</td>
-                  <td className="tc"><FaCheck className="small_icon green" /></td>
-                  <td className="tc"><FaCheck className="small_icon green" /></td>
-                </tr>
-                <tr>
-                  <td>Manage Attendees</td>
-                  <td className="tc"><FaCheck className="small_icon green" /></td>
-                  <td className="tc"><FaCheck className="small_icon green" /></td>
-                </tr>
-                <tr>
-                  <td>We Pledge Not to Cancel Your Events</td>
-                  <td className="tc"><FaCheck className="small_icon green" /></td>
-                  <td className="tc"><FaCheck className="small_icon green" /></td>
-                </tr>
-              </tbody>
-            </table>
+                    <td className="tc"><FaCheck className="small_icon green" /></td>
+                    <td className="tc"><FaCheck className="small_icon green" /></td>
+                  </tr>
+                  <tr>
+                    <td>Sell Tickets</td>
+                    <td className="tc"><FaCheck className="small_icon green" /></td>
+                    <td className="tc"><FaCheck className="small_icon green" /></td>
+                  </tr>
+                  <tr>
+                    <td>Run SMS Campaigns</td>
+                    <td className="tc"><FaCheck className="small_icon green" /></td>
+                    <td className="tc"><FaXmark className="small_icon red" /></td>
+                  </tr>
+                  <tr>
+                    <td>Easily create custom dynamic seating charts</td>
+                    <td className="tc"><FaCheck className="small_icon green" /></td>
+                    <td className="tc"><FaXmark className="small_icon red" /></td>
+                  </tr>
+                  <tr>
+                    <td>Manage Orders</td>
+                    <td className="tc"><FaCheck className="small_icon green" /></td>
+                    <td className="tc"><FaCheck className="small_icon green" /></td>
+                  </tr>
+                  <tr>
+                    <td>Manage Attendees</td>
+                    <td className="tc"><FaCheck className="small_icon green" /></td>
+                    <td className="tc"><FaCheck className="small_icon green" /></td>
+                  </tr>
+                  <tr>
+                    <td>We Pledge Not to Cancel Your Events</td>
+                    <td className="tc"><FaCheck className="small_icon green" /></td>
+                    <td className="tc"><FaCheck className="small_icon green" /></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
           </div>
-        </div>
         </motion.div>
 
 
